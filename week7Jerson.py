c5f79aefc68266bc62984268f69eba656877a132
jerson2257 <jfernandez_230000002257@uic.edu.ph>
2026-09-09 01:53:28 +0000

    xmltodict not installed

--- week7Jerson.py
+++ week7Jerson.py
@@ -1,1 +1,10 @@
+import json
+import yaml
+import os
 
+try:
+    import xmltodict
+    HAS_XML_TODICT = True
+except:
+    HAS_XML_TODICT = False
+    print("Note: xmltodict not installed. Run: pip install xmltodict")
