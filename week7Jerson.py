3b47f7d4780f57fd059465ab157913ec3e797932
jerson2257 <jfernandez_230000002257@uic.edu.ph>
2026-09-09 01:59:53 +0000

    Updated ImportError:

--- week7Jerson.py
+++ week7Jerson.py
@@ -5,6 +5,6 @@ import os
 try:
     import xmltodict
     HAS_XML_TODICT = True
-except:
+except ImportError:
     HAS_XML_TODICT = False
     print("Note: xmltodict not installed. Run: pip install xmltodict")
